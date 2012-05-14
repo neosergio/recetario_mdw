bfade5e06f95f7d7efdf917deb72764429d52baa
Sergio Infante <raulsergio9@gmail.com>
2012-05-13 21:42:40 -0500

    Arreglando los modelos

--- recetario/principal/admin.py
+++ recetario/principal/admin.py
@@ -1,5 +1,5 @@
-from principal.models import Bebida, Receta
+from principal.models import Receta, Comentario
 from django.contrib import admin
 
-admin.site.register(Bebida)
 admin.site.register(Receta)
+admin.site.register(Comentario)
--- recetario/principal/models.py
+++ recetario/principal/models.py
@@ -2,21 +2,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Bebida(models.Model):
-	nombre = models.CharField(max_length=50)
-	ingredientes = models.TextField()
-	preparacion = models.TextField()
-
-	def __unicode__(self):
-		return self.nombre
-
 class Receta(models.Model):
 	titulo = models.CharField(max_length=100, verbose_name='Título', unique=True)
 	ingredientes = models.TextField(help_text='Redacta los ingredientes')
-	prepacion = models.TextField(verbose_name='Preparación')
+	preparacion = models.TextField(verbose_name='Preparación', help_text='El proceso de preparación')
 	imagen = models.ImageField(upload_to='recetas', verbose_name='Imágen')
 	tiempo_registro = models.DateTimeField(auto_now=True)
 	usuario = models.ForeignKey(User)
 	
 	def __unicode__(self):
 		return self.titulo
+
+class Comentario(models.Model):
+    receta = models.ForeignKey(Receta)
+    texto = models.TextField(help_text='Tu comentario', verbose_name='Comentario')
+
+    def __unicode__(self):
+        return self.texto
