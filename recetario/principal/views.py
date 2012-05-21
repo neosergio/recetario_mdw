6f72b13d6e89e68b574ab2d9df0d8d0a85662408
Sergio Infante <raulsergio9@gmail.com>
2012-05-20 20:38:44 -0500

    Cambios para las vistas

--- recetario/principal/views.py
+++ recetario/principal/views.py
@@ -1,6 +1,21 @@
-from principal.models import Bebida
-from django.shortcuts import render_to_response
+from principal.models import Receta, Comentario
+from django.contrib.auth.models import User
+from django.shortcuts import render_to_response, get_object_or_404
 
-def lista_bebidas(request):
-	bebidas = Bebida.objects.all()
-	return render_to_response('lista_bebidas.html',{'lista':bebidas})
+def inicio(request):
+    recetas = Receta.objects.all()
+    return render_to_response('inicio.html',{'recetas':recetas})
+
+def usuarios(request):
+    usuarios = User.objects.all()
+    recetas = Receta.objects.all()
+    return render_to_response('usuarios.html',{'usuarios':usuarios,'recetas':recetas})
+
+def lista_recetas(request):
+    recetas = Receta.objects.all()
+    return render_to_response('recetas.html',{'datos':recetas})
+
+def detalle_receta(request, id_receta):
+    dato = get_object_or_404(Receta, pk=id_receta)
+    comentarios = Comentario.objects.filter(receta=dato)
+    return render_to_response('receta.html',{'receta':dato,'comentarios':comentarios})
--- recetario/recetario/urls.py
+++ recetario/recetario/urls.py
@@ -5,7 +5,10 @@ from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^$','principal.views.lista_bebidas'),
+	url(r'^$','principal.views.inicio'),
+        url(r'^usuarios/$','principal.views.usuarios'),
+        url(r'^recetas/$','principal.views.lista_recetas'),
+        url(r'^receta/(?P<id_receta>\d+)$','principal.views.detalle_receta'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$','django.views.static.serve',
