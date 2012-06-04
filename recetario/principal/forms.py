4cf4098e4f09aa2e6c594574969ba96b1f7480b3
Sergio Infante <raulsergio9@gmail.com>
2012-06-03 21:06:07 -0500

    Cambios temporales

---
+++ recetario/principal/forms.py
@@ -0,0 +1,18 @@
+#encoding:utf-8 
+
+from django.forms import ModelForm
+from django import forms
+from principal.models import Receta, Comentario
+
+
+class RecetaForm(ModelForm):
+    class Meta:
+        model = Receta
+
+class ComentarioForm(ModelForm):
+    class Meta:
+        model = Comentario
+
+class ContactoForm(forms.Form):
+	correo = forms.EmailField(label='Tu correo electrónico')
+	mensaje = forms.CharField(widget='forms.Textarea')
--- recetario/principal/views.py
+++ recetario/principal/views.py
@@ -1,9 +1,9 @@
 from principal.models import Receta, Comentario
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-
+from principal.forms import RecetaForm, ComentarioForm, ContactoForm
 
 def sobre(request):
 	html = "<html><body>Proyecto de ejemplo en MDW</body></html>"
@@ -26,3 +26,23 @@ def detalle_receta(request, id_receta):
     dato = get_object_or_404(Receta, pk=id_receta)
     comentarios = Comentario.objects.filter(receta=dato)
     return render_to_response('receta.html',{'receta':dato,'comentarios':comentarios}, context_instance=RequestContext(request))
+
+def nueva_receta(request):
+    if request.method=='POST':
+        formulario = RecetaForm(request.POST, request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return HttpResponseRedirect('/recetas')
+    else:
+        formulario = RecetaForm()
+    return render_to_response('recetaform.html',{'formulario':formulario},
+    context_instance=RequestContext(request))
+
+def contacto(request):
+    if request.method=='POST':
+        formulario = ContactoForm(request.POST)
+        if formulario.is_valid():
+            print formulario
+    else:
+        formulario = ContactoForm()
+    return render_to_response('contactoform.html',{'formulario':formulario}, context_instance=RequestContext(request))
